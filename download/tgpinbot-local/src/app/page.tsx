a63f2acd4b6c444ef4dc7664de5bb1acb59ca83d
Z User <z@container>
2026-03-09 00:55:34 +0000

    Fix Progress scroll - use ScrollArea like Premium

--- download/tgpinbot-local/src/app/page.tsx
+++ download/tgpinbot-local/src/app/page.tsx
@@ -613,8 +613,9 @@ export default function PinterestApp() {
           </TabsContent>
 
           {/* Progress Tab */}
-          <TabsContent value="progress" className="mt-4 flex-1 min-h-0 overflow-y-auto data-[state=inactive]:hidden">
-            <div className="space-y-4 pr-2 pb-4">
+          <TabsContent value="progress" className="mt-4 flex-1 flex flex-col overflow-hidden data-[state=inactive]:hidden">
+            <ScrollArea className="flex-1 h-0">
+              <div className="space-y-4 pr-2 pb-4">
               {/* Stats */}
               <div className="grid grid-cols-3 gap-3">
                 <div className="gradient-pink rounded-lg p-3 text-center">
@@ -662,6 +663,7 @@ export default function PinterestApp() {
                 </CardContent>
               </Card>
             </div>
+            </ScrollArea>
           </TabsContent>
 
           {/* Premium Tab */}
